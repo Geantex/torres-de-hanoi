f38b96ede4adcad27f6307b8f8099112e0209938
Mr. Beast <lavafrita@gmail.com>
2023-03-01 15:40:53 +0100

    Mas o menos mover disco

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -11,17 +11,39 @@ namespace Torres_de_Hanoi
         /*TODO: Implementar métodos*/
         public void mover_disco(Pila a, Pila b)
         {
-            bool moverDisco; // Se ha movido el disco??
+            bool moverDisco = false; // Se ha movido el disco??
             // Pila a es la pila donde cogemos el disco, Pila b es donde lo dejamos
             // Podria ser util que push() devuelva verdadero o falso dependiendo de si ha podido mover el disco
+            if(a.Size != 0){
              moverDisco = b.push(a.Elementos.Last());
+            }
              if(moverDisco){
-                a.pop();      
+                a.pop();  
+                Console.WriteLine("Probando!");   
              }
+
+             // Fin de la función
+             return;
         }
 
         public int iterativo(int n, Pila ini, Pila fin, Pila aux)
         {
+            if(n == 1) {
+                mover_disco(ini,fin);
+            }
+            else{
+                // Hay que mover todos menos un disco a AUX,
+                // después movemos el único disco que queda en INI a FIN,
+                // y movemos de la misma manera todos los discos a FIN
+                mover_disco(ini,aux);
+                mover_disco(ini,fin);
+                mover_disco(aux,fin);
+                while(ini.Size > 1){
+                    mover_disco(ini,aux);
+                    mover_disco(fin,ini);
+                    mover_disco(fin,aux);
+                }
+            }
             return 0;
         }
 
